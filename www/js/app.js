77e970205a13a9704a65d6b7815e40866a43c811
Tomasz Gabrysiak <tomsonwch@gmail.com>
2015-10-18 17:02:45 +0200

    complete cat-app
    Only index.html, app.js and style.css were changed manually.

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,25 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.controller('CatController', ['$scope', function($scope) {
+  // default image url, note that gifs mode is enabled by default
+  $scope.imageUrl = 'http://thecatapi.com/api/images/get?format=src&type=gif';
+  // 'gifs mode', enabled by default
+  $scope.toggleModel = {
+    gifs: true
+  };
+  // loads next image
+  // 't' parameter is different each time the function is triggered. This is done
+  // to force the browser to reload the image and not to use cache
+  $scope.reload = function() {
+    $scope.imageUrl = "http://thecatapi.com/api/images/get?format=src&type="
+      + ($scope.toggleModel.gifs ? "gif" : "jpg,png") + "&t=" + new Date().getTime();
+  };
+}]);
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +32,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
